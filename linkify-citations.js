abc16189abee4bfda3079c6fa7264c0869113999
Alan deLevie <adelevie@gmail.com>
2015-09-18 15:24:33 -0400

    Initial commit

---
+++ linkify-citations.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var citationToPFLink = function(citation) {
+    if (citation.type == "reporter") {
+      var url = "/" + citation.reporter.volume + "/" + citation.reporter.reporter + "/" + citation.reporter.page;
+      return "<a class='citation' href='" + url + "'>" + citation.match + "</a>";
+    };
+  };
+  
+  //var thePage = $('div#fetched_page');
+  var doc = document.getElementById('fetched_page');
+  var thePage = doc;
+  // find the citations
+  var citations = Citation.find(doc.innerHTML).citations;
+              
+  // loop through each citation
+  for (i = 0; i < citations.length; i++) {
+    if (citations[i].type == "reporter") {
+      // generate a link
+      var link = citationToPFLink(citations[i]);
+      // stick the link onto the DOM
+      thePage.innerHTML = thePage.innerHTML.replace(citations[i].match, link);
+    } 
+  }
+});
